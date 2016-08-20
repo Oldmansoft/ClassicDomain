ebd4fcf9b18aa1d6f916b0812504cf7cb3d9385d
Oldman <angelialsong@gmail.com>
2016-08-20 23:48:51 +0800

    添加 Entity Framework 测试单元。

---
+++ src/UnitTest.ClassicDomain/Drvier/UtEntityFrameworkDomainOperate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.ClassicDomain.Drvier.EntityFramework
+{
+    [TestClass]
+    public class UtEntityFrameworkDomainOperate
+    {
+        [TestMethod]
+        public void TestAddReplaceRemove()
+        {
+            var factory = new Factory();
+            var repository = factory.CreateBook();
+
+            var domain = new Domain.Book();
+            domain.Name = "hello";
+            repository.Add(domain);
+            factory.GetUnitOfWork().Commit();
+
+            domain = repository.Get(domain.Id);
+            Assert.IsNotNull(domain);
+
+            domain.Name = "world";
+            repository.Replace(domain);
+            factory.GetUnitOfWork().Commit();
+
+            domain = repository.Get(domain.Id);
+            Assert.AreEqual("world", domain.Name);
+
+            repository.Remove(domain);
+            factory.GetUnitOfWork().Commit();
+
+            domain = repository.Get(domain.Id);
+            Assert.IsNull(domain);
+        }
+    }
+}
