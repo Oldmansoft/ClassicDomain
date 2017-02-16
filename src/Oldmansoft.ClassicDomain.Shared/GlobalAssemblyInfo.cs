1b702ed613b5873abe3b202a04151cd3ebe04ec0
Oldman <angelialsong@gmail.com>
2017-02-16 23:09:49 +0800

    手动更改生成号。

--- src/Oldmansoft.ClassicDomain.Shared/GlobalAssemblyInfo.cs
+++ src/Oldmansoft.ClassicDomain.Shared/GlobalAssemblyInfo.cs
@@ -2,20 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("3.0.54")]
